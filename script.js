e503dd9edfdb47053bcfa1e4507c18c0c16e47ee
beat-the-lag <67851753+beat-the-lag@users.noreply.github.com>
2020-07-05 04:51:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,17 +18,31 @@ function save_preferences() {
     localStorage.setItem("caffeine_p", "Yes");
   } else {
     localStorage.setItem("caffeine_p", "No");
-  };
+  }
   /*get everything else (lat and long updated with map in autofill_script.js)*/
-  localStorage.setItem("depart_city", document.getElementById("autocomplete").value);
-  localStorage.setItem("arrive_city", document.getElementById("autocomplete2").value);
-  localStorage.setItem("departure_string", document.getElementById("d_time").value);
-  localStorage.setItem("arrival_string", document.getElementById("a_time").value);
+  localStorage.setItem(
+    "depart_city",
+    document.getElementById("autocomplete").value
+  );
+  localStorage.setItem(
+    "arrive_city",
+    document.getElementById("autocomplete2").value
+  );
+  localStorage.setItem(
+    "departure_string",
+    document.getElementById("d_time").value
+  );
+  localStorage.setItem(
+    "arrival_string",
+    document.getElementById("a_time").value
+  );
   localStorage.setItem("sleep_start", document.getElementById("sleep").value);
   localStorage.setItem("sleep_end", document.getElementById("wake").value);
-  localStorage.setItem("caffeine_excess", document.getElementById("caffeine_excess").checked);
-  
-};
+  localStorage.setItem(
+    "caffeine_excess",
+    document.getElementById("caffeine_excess").checked
+  );
+}
 function schedules_initialize() {
   document.getElementById("p_shift").innerHTML = localStorage.getItem(
     "shift_p"
@@ -43,107 +57,124 @@ function schedules_initialize() {
     "caffeine_p"
   );
   generate_schedules();
-};
-function generate_schedules(){
-  function revert(v){
+}
+function generate_schedules() {
+  function revert(v) {
     /*converts any hours outside [0-24] to between regular hours*/
-    if (v<0){
+    if (v < 0) {
       v += 24;
-    } else if (v>=24){
+    } else if (v >= 24) {
       v -= 24;
-    };
-    return v
-  };
-  
+    }
+    return v;
+  }
+
   /* STEP 1: DEPARTURE/ARRIVAL DATE AND TIME */
   var dep_date = new Date(localStorage.getItem("departure_string"));
   var dep_time = dep_date.getTime() / 1000;
   var arr_date = new Date(localStorage.getItem("arrival_string"));
   var arr_time = arr_date.getTime() / 1000;
-  
+
   /* STEP 2: TIMEZONE OF DEPARTURE AND ARRIVAL LOCATIONS */
-  var depart_tz = Math.floor(parseFloat(localStorage.getItem("depart_city_lng"))/15);
-  var arrive_tz = Math.floor(parseFloat(localStorage.getItem("arrive_city_lng"))/15);
+  var depart_tz = Math.floor(
+    parseFloat(localStorage.getItem("depart_city_lng")) / 15
+  );
+  var arrive_tz = Math.floor(
+    parseFloat(localStorage.getItem("arrive_city_lng")) / 15
+  );
   var delta = Math.abs(arrive_tz - depart_tz);
   //alert(delta);
-  
+
   /* STEP 3: SLEEP SCHEDULE */
   var sleep_start0 = localStorage.getItem("sleep_start");
   var sleep_end0 = localStorage.getItem("sleep_end");
-  var sleep_start = parseInt(sleep_start0.slice(0,2));
-  var sleep_end = parseInt(sleep_end0.slice(0,2));
+  var sleep_start = parseInt(sleep_start0.slice(0, 2));
+  var sleep_end = parseInt(sleep_end0.slice(0, 2));
   var lbt;
-  if (sleep_end<sleep_start){
+  if (sleep_end < sleep_start) {
     if (sleep_end - sleep_start + 24 < 7) {
-      lbt = sleep_end-2;
+      lbt = sleep_end - 2;
     } else {
-      lbt = sleep_end-3;
-    };
-  } else if (sleep_end-sleep_start<7) {
-    lbt = sleep_end-2;
+      lbt = sleep_end - 3;
+    }
+  } else if (sleep_end - sleep_start < 7) {
+    lbt = sleep_end - 2;
   } else {
-    lbt = sleep_end-3;
-  };
+    lbt = sleep_end - 3;
+  }
   lbt = revert(lbt);
-  
+
   /* STEP 4 : SHIFT DAY */
   var shift_day = new Date(localStorage.getItem("shift_p"));
   var shift_time = shift_day.getTime() / 1000;
-  if (shift_time>dep_time){
+  if (shift_time > dep_time) {
     shift_time = dep_time;
-  };
-  
-  var days_before = parseInt(Math.floor((dep_time-shift_time)/86400));
-  localStorage.setItem("days_before",days_before);
-  
-  
-  var change = Math.floor(delta/days_before);
-  
+  }
+
+  var days_before = parseInt(Math.floor((dep_time - shift_time) / 86400));
+  localStorage.setItem("days_before", days_before);
+
+  var change = Math.floor(delta / days_before);
+
   /* SCHEDULE */
   var schedules = [];
-  function schedule(index, change, sleep_start, sleep_end, lbt){
+  function schedule(index, change, sleep_start, sleep_end, lbt) {
     var temp = [];
-    for (const day of Array(index).keys()){
-      lbt = revert(lbt+change);
-      sleep_start = revert(sleep_start+change);
-      sleep_end = revert(sleep_end+change);
-      temp.push([sleep_start,sleep_end,revert(sleep_start+delta),revert(sleep_end+delta)]);
+    for (const day of Array(index).keys()) {
+      lbt = revert(lbt + change);
+      sleep_start = revert(sleep_start + change);
+      sleep_end = revert(sleep_end + change);
+      temp.push([
+        sleep_start,
+        sleep_end,
+        revert(sleep_start + delta),
+        revert(sleep_end + delta)
+      ]);
     }
     //add score to temp
     // coffee alcohol travel_sleep
-    if (localStorage.getItem("caffeine_excess") == true){
+    if (localStorage.getItem("caffeine_excess") == true) {
       var coffee = -1;
-    } else if (localStorage.getItem("caffeine_p") == "Yes"){
+    } else if (localStorage.getItem("caffeine_p") == "Yes") {
       var coffee = 1;
     } else {
       var coffee = 0;
-    };
-    if (localStorage.getItem("alcohol_p") == "Yes"){
+    }
+    if (localStorage.getItem("alcohol_p") == "Yes") {
       var alcohol = 1;
     } else {
       var alcohol = 0;
-    };
+    }
     var travel_sleep = 0; //for now
-    
+
     temp.push(score(days_before, change, coffee, alcohol, travel_sleep));
     schedules.push(temp); //array with arrays of schedules with days represented by arrays
-    
-    
-  };
-  function score(days_before, delta, coffee, alcohol, travel_sleep){
-    return (Math.max(days_before, Math.abs(delta)) + coffee*0.2 + alcohol*0.2 + travel_sleep*0.2);
-  };
-  schedule(days_before,change,sleep_start,sleep_end,lbt);
+  }
+  function score(days_before, change, coffee, alcohol, travel_sleep) {
+    return (
+      100 -
+      (Math.abs(delta) +
+        (change / days_before) * 9 +
+        coffee * 0.5 +
+        alcohol * 0.7 +
+        travel_sleep * 0.5)
+    );
+  }
+  schedule(days_before, change, sleep_start, sleep_end, lbt);
   //2nd
-  schedule(days_before,change,sleep_start,sleep_end,lbt); // this should add another schedule
+  schedule(delta, 1, sleep_start, sleep_end, lbt); // this should add another schedule this should be the ideal schedule
   //3rd
-  schedule(days_before,change,sleep_start,sleep_end,lbt); // this should add another schedule
+  schedule(delta / 2, 2, sleep_start, sleep_end, lbt); // this should add another schedule not as good as above but is more reasonable
 
-  
   localStorage.setItem("schedules", JSON.stringify(schedules));
   show_schedules();
-};
+}
 
-function show_schedules(){
+function show_schedules() {
   var schedules = JSON.parse(localStorage.getItem("schedules"));
+}
+
+function d_schedules() {
+  document.getElementById("rec_display").style.display = "block";
+  document.getElementById("rec_btn").style.display = "none";
 }
